e12d7ba30da9f769c504404f9ff9f0ac0b948460
nDim <themiindtrip@gmail.com>
2026-03-14 13:31:37 -0400

    Add Diff2FlowEulerSampler to module exports in __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,9 @@
 from .attention import AsymmetricRoPECrossAttention
 from .bridge import CausalToSpatialPerceiverBridge
+from .sampler import Diff2FlowEulerSampler
 
 __all__ = [
     "AsymmetricRoPECrossAttention",
     "CausalToSpatialPerceiverBridge",
+    "Diff2FlowEulerSampler",
 ]
