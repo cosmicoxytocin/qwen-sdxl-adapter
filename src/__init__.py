90b970a10a230d0e1e00be936397f87363573095
nDim <themiindtrip@gmail.com>
2026-03-14 13:32:25 -0400

    Add Diff2FlowEulerSampler to module exports in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from .config import ExperimentConfig
 from .models.bridge import CausalToSpatialPerceiverBridge
 from .models.attention import AsymmetricRoPECrossAttention
+from .models.sampler import Diff2FlowEulerSampler
 from .training.trainer import AdapterTrainer
 from .training.loss import Diff2FlowAlignmentLoss
 from .data.dataset import CachedAdapterDataset, create_dataloader
@@ -10,6 +11,7 @@ __all__ = [
     "ExperimentConfig",
     "CausalToSpatialPerceiverBridge",
     "AsymmetricRoPECrossAttention",
+    "Diff2FlowEulerSampler",
     "AdapterTrainer",
     "Diff2FlowAlignmentLoss",
     "CachedAdapterDataset",
